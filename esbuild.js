d59d47a5e69ff41786d822452600f6a072bebde4
spencercap <cappiello.spencer@gmail.com>
2022-03-19 15:52:16 +0000

    outExtension .mjs test

--- esbuild.js
+++ esbuild.js
@@ -9,6 +9,7 @@ esbuild
 		minify: false,
 		splitting: true, // only for esm
 		format: 'esm', // esm works in node+browser
+		// outExtension: { '.js': '.mjs' }, // .js -> .mjs (change package.json main + modules entry IF doing this)
 		target: ['esnext']
 	})
 	.catch(() => process.exit(1));

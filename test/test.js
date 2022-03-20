a308bb05b551796ac09632f881ee023f08fc5d59
spencercap <cappiello.spencer@gmail.com>
2022-03-20 23:17:20 +0000

    mjs only requires other repo's to use .mjs files or type: "module" \: not ideal

---
+++ test/test.js
@@ -0,0 +1,7 @@
+"use strict";
+// test.ts
+exports.__esModule = true;
+var index_1 = require("../dist/index");
+console.log('starting test');
+var sum = index_1.add(5, 6);
+console.log('sum', sum);
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+// test.ts
+
+import { add } from '../dist';
+
+console.log('starting test');
+
+const sum = add(5, 6);
+console.log('sum', sum);

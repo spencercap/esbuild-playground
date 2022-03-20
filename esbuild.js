fca5de863c773a02fee193390125395272894aa8
spencercap <cappiello.spencer@gmail.com>
2022-03-20 23:17:20 +0000

    mjs only requires other repo's to use .mjs files or type: "module" \: not ideal

--- esbuild.js
+++ esbuild.js
@@ -1,15 +1,14 @@
-const esbuild = require('esbuild');
+import { build } from 'esbuild';
 
-esbuild
-	.build({
-		entryPoints: ['src/index.ts'],
-		outdir: 'dist',
-		bundle: true,
-		sourcemap: true,
-		minify: false,
-		splitting: true, // only for esm
-		format: 'esm', // esm works in node+browser
-		// outExtension: { '.js': '.mjs' }, // .js -> .mjs (change package.json main + modules entry IF doing this)
-		target: ['esnext']
-	})
+build({
+	entryPoints: ['src/index.ts'],
+	outdir: 'dist',
+	bundle: true,
+	sourcemap: true,
+	minify: false,
+	splitting: true, // only for esm
+	format: 'esm', // esm works in node+browser
+	// outExtension: { '.js': '.mjs' }, // .js -> .mjs (change package.json main + modules entry IF doing this)
+	target: ['esnext']
+})
 	.catch(() => process.exit(1));

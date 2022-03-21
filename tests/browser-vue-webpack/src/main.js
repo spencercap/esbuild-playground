50ce642578fc7b797ad50ae1ba4a83f1836f34c8
spencercap <cappiello.spencer@gmail.com>
2022-03-21 16:53:53 +0000

    added test for browser + node

---
+++ tests/browser-vue-webpack/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// test
+console.log('!!!!');
+
+import { add } from 'esbuild-playground';
+import * as pkg from 'esbuild-playground'; // works
+// import { add } from './node_modules/esbuild-playground/dist/index.mjs'; // works
+// const { add } = require('esbuild-playground');
+
+console.log(pkg);
+console.log(add);
+
+console.log('starting test');
+
+// const sum = pkg.add(5, 6);
+const sum = add(5, 6);
+console.log('sum', sum);
